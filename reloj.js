97e1129b1c73b41d73473d9ae238fa32e6ea48f6
jorgellose <jorgellose@gmail.com>
2017-06-10 11:44:45 +0200

    Reloj terminado

--- reloj.js
+++ reloj.js
@@ -1,9 +1,9 @@
-var reloj;
 cargarHoraReloj();
+var intervaloReloj = setInterval(cargarHoraReloj, 1000);
 
 //https://www.w3schools.com/jsref/jsref_obj_date.asp
 function cargarHoraReloj() {
-    reloj = document.getElementById("reloj");
+    var reloj = document.getElementById("reloj");
     var fechaActual = new Date();
     var horas = fechaActual.getHours();
     if (horas < 10) {
@@ -18,4 +18,16 @@ function cargarHoraReloj() {
         segundos = "0" + segundos
     }
     reloj.innerHTML = horas + ":" + minutos + ":" + segundos;
+}
+
+function pararReloj(boton) {
+    if (boton.innerHTML === "Parar hora") {
+        boton.innerHTML = "Activar hora";
+        clearInterval(intervaloReloj);
+    }
+    else {
+        boton.innerHTML = "Parar hora";
+        cargarHoraReloj();
+        intervaloReloj = setInterval(cargarHoraReloj, 1000);
+    }
 }
