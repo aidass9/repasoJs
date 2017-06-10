301f98e5141a6cd539d9c1a2c1de9b4f658eed45
jorgellose <jorgellose@gmail.com>
2017-06-10 11:02:04 +0200

    La tabla muestra los datos

--- index.js
+++ index.js
@@ -18,18 +18,47 @@ function cargarDatos() {
     })
 }
 function dividirPorLineas(datos) {
-    lineas = datos.split(/\n/);
+    var lineas = datos.split(/\n/);
     for (i = 0; i < lineas.length; i++) {
         dividirPorCampo(lineas[i])
         //console.log(i+") "+lineas[i]) //Muestra por consola cada linea
     }
+    cargarDatosTabla(true);
 }
 
 function dividirPorCampo(linea) {
     campos = linea.split(";");
-    id.push(campos[0]);
+    ids.push(campos[0]);
     nombres.push(campos[1]);
     udcaja.push(campos[2]);
     precio.push(campos[3]);
 }
 
+function cargarDatosTabla(condicion) {
+    //reseteamos tabla
+    document.getElementById("tabla").innerHTML = ""
+
+    //ingresamos datos
+    for (i = 0; i < ids.length; i++) {
+        if (condicion === true) {
+            insertarFila(i);
+        }
+        else if (ids[i].charAt(0) === "1") {
+            //Si la condición es falsa y la primera letra de la id es 1 entra
+            insertarFila(i);
+        }
+    }
+}
+
+function insertarFila(indice) {
+    var filaTabla = `
+    <tr>
+        <td>${ids[indice]}</td>
+        <td>${nombres[indice]}</td>
+        <td>${udcaja[indice]}</td>
+        <td>${precio[indice]}</td>
+    </tr>
+    `
+    document.getElementById("tabla").innerHTML += filaTabla
+
+}
